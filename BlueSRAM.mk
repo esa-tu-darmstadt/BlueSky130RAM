303d986e124339a1d0bcbdc6d032bb49cfa4f876
Markus Scheck <scheck@esa.tu-darmstadt.de>
2024-04-12 16:01:57 +0200

    write out makefile snippet to directly include simulation model and ignore missing timescale for openRAM

--- BlueSRAM.mk
+++ BlueSRAM.mk
@@ -2,3 +2,8 @@ MAKEPATH := $(dir $(lastword $(MAKEFILE_LIST)))
 MODULENAME := BlueBRAM
 MODULEPATH := $(MAKEPATH)src
 EXTRA_BSV_LIBS += $(MODULEPATH)
+
+RUN_FLAGS+= -suppress 3009
+-include openram.mk
+
+$(info Adding $(MODULENAME) from $(MODULEPATH))
